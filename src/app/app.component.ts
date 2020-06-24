fb8ef4bf32c85b92734541674d9968223567e07f
George Pagonoudis <giorgospago23@gmail.com>
2020-06-24 19:56:47 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'loadmore';
+export class AppComponent implements OnInit {
+  public limit = 10;
+  public counter = 0;
+
+  public boxes = [];
+
+  @HostListener("window:scroll",)
+  public onScroll() {
+    const fullHeight = document.documentElement.scrollHeight;
+    const scrollTop = document.documentElement.scrollTop;
+    const screenHeight = document.documentElement.clientHeight;
+    const rest = fullHeight - (screenHeight + scrollTop);
+
+    if (rest < 80) {
+      this.pushNamesToArray();
+    }
+  }
+
+  ngOnInit() {
+    this.pushNamesToArray();
+  }
+
+  pushNamesToArray() {
+    console.log("skip: " + (this.counter * this.limit) + ", limit: " + this.limit);
+
+    for (let i = 0; i < 10; i++) {
+      this.boxes.push("Name " + i);
+    }
+
+    this.counter++;
+  }
+
 }
